fdd16492c0b9125fb50e253d6c1f0422376af3b7
zanjs <root@zanjs.com>
2016-01-01 16:08:58 +0800

    :dog: index.js path

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 var css =  require('./scss/main.scss');
 
-var sub = require('./sub');
+var sub = require('./js/sub');
 var app  = document.createElement('div');
 app.innerHTML = '<h1>Hello World index</h1>';
 app.appendChild(sub());
