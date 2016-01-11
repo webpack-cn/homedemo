0051de80500e43a2bd3bae079105107f5f96aca0
zanjs <root@zanjs.com>
2016-01-11 15:35:17 +0800

    :dog:  miao

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,16 @@
+'use strict';
 
+var webpack = require('webpack');
 var path = require('path');
+
+var fs = require('fs');
+// 定义函数判断是否是在当前生产环境，这个很重要，一位开发环境和生产环境配置上有一些区别
+var isProduction = function () {
+  return process.env.NODE_ENV === 'production';
+};
+
+
+
 var HtmlwebpackPlugin = require('html-webpack-plugin');
 //定义了一些文件夹的路径
 var ROOT_PATH = path.resolve(__dirname);
@@ -27,7 +38,7 @@ module.exports = {
         loader: 'babel',
         include: APP_PATH,
         query: {
-          presets: ['es2015']
+          presets: ["es2015"]
         }
       },
       {
@@ -39,7 +50,10 @@ module.exports = {
   //添加我们的插件 会自动生成一个html文件
   plugins: [
     new HtmlwebpackPlugin({
-      title: 'Hello webpack app'
+      title: 'Hello webpack app',    
+      filename: 'index.html',
+      template: path.join(__dirname, '/app/temp/index.html'),
+      inject: 'body'
     })
   ]
 };
